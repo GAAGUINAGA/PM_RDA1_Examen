4de78423bc102f24dbddbcc25c6aca8804e5a90b
alex171215 <alexandercarrion918@gmail.com>
2026-09-25 09:13:11 -0500

    feat: ui state

--- app/src/main/java/com/example/safepass2026/ui/RegistroState.kt
+++ app/src/main/java/com/example/safepass2026/ui/RegistroState.kt
@@ -1,2 +1,15 @@
 package com.example.safepass2026.ui
 
+import com.example.safepass2026.data.Asistente
+
+sealed class RegistroState{
+    data object Idle:RegistroState()
+
+    data class Success(
+        val asistente: Asistente,
+        val pasePrioritario: Boolean,
+        val mensaje: String
+    ) : RegistroState()
+
+    data class Error(val causa: String) : RegistroState()
+}
