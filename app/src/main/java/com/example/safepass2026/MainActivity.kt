630a00163074768dbbe2b873a132ceb6f483a7f4
Paúl Alexander Rosero Carrión <alexandercarrion918@gmail.com>
2026-09-25 16:00:50 -0500

    Refactor MainActivity to use SafePassScreen

--- app/src/main/java/com/example/safepass2026/MainActivity.kt
+++ app/src/main/java/com/example/safepass2026/MainActivity.kt
@@ -1,46 +1,21 @@
 package com.example.safepass2026
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.safepass2026.ui.theme.Laboratorio1Theme
+import com.example.safepass2026.ui.SafePassScreen
+import androidx.compose.material3.MaterialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Cumplimiento estricto del estándar Android 16 (API 36): Dibujado de borde a borde
         enableEdgeToEdge()
         setContent {
-            Laboratorio1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            MaterialTheme {
+                SafePassScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Laboratorio1Theme {
-        Greeting("Android")
     }
 }
